2bf5d758e9ece6956b94aeac2615b81222597f08
DanieleMorello <danielemorello@live.it>
2023-03-14 15:14:18 +0100

    Create on Js: container, row, col and img

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,7 @@ creare un carosello come nello screenshot allegato.
   ma costruito dinamicamente attraverso JavaScript.
 */
 
-// Dato un array contenente una lista di cinque immagini
+// Definiamo l'array di immagini
 const images = [
   "01.webp",
   "02.webp",
@@ -21,3 +21,27 @@ const images = [
   "04.webp",
   "05.webp"
 ];
+
+// Selezioniamo l'elemento body nella pagina HTML
+const body = document.querySelector("body");
+
+// Creiamo un elemento div con la classe container
+const containerDiv = document.createElement("div");
+containerDiv.classList.add("container");
+body.appendChild(containerDiv);
+
+// Creiamo un elemento div con la classe row e lo aggiungiamo al container
+const rowDiv = document.createElement("div");
+rowDiv.classList.add("row");
+containerDiv.appendChild(rowDiv);
+
+// Creiamo un elemento div con la classe col e lo aggiungiamo alla row
+const colDiv = document.createElement("div");
+colDiv.classList.add("col");
+rowDiv.appendChild(colDiv);
+
+// Creiamo un tag img e lo aggiungiamo alla colonna
+const imgTag = document.createElement("img");
+imgTag.setAttribute("src", `./assets/img/${images[0]}`);
+imgTag.setAttribute("alt", "");
+colDiv.appendChild(imgTag);
